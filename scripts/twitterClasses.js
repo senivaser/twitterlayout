0a6a195ef5b4b32ce85d587854753a66c6037c0a
senivaser <senivaser@gmail.com>
2021-01-21 10:17:14 +0300

    Day2 Edited

--- scripts/twitterClasses.js
+++ scripts/twitterClasses.js
@@ -1,6 +1,8 @@
+
+//Класс работы с запросами
 class FetchData {
 
-  //
+  //Общая функция для получения данных из запроса
   getResourse = async url => {
     const res = await fetch(url)
 
@@ -11,37 +13,63 @@ class FetchData {
     return(res.json())
   }
 
+  //Получение всех постов из запроса
   getPost = async () => this.getResourse('db/dataBase.json')
 
 }
 
-
+//Основной класс страницы-твиттера, содержащий
+//в себе перечень и фунцкионал постов в собственной ленте 
 class Twitter {
+
   constructor(param) {
+
+    //Создается объект для работы с запросами
     const fetchData = new FetchData()
+
+    //Характеристика является объектом класса Posts,
+    //по сути содержит все посты и методы воздействия на них
     this.tweets = new Posts()
+
+    //Указание класса тега родителя для вставки поста
     this.elements = {
       listElem: document.querySelector(param.listElem)
     }
 
+    //Заполнение this.tweets постами из данных запроса
     fetchData.getPost()
       .then(data => {
         data.forEach(this.tweets.addPost)
+
+        //По умолчанию, при создании объекта Twitter происходит 
+        //отображение всех постов на странице, так как вызов 
+        //этой функции выполняется в конструкторе
         this.showAllPosts()
       })
       
     
-    console.log(this.tweets)
+    //console.log(this.tweets)
   }
 
+  //Функция непосредственно отображает посты в верстке, переданные
+  // в аргументе, через HTML-вставки и является инструментом для 
+  //других функций отображения
   renderPosts(posts) {
 
+    //Удаление текста внутри родительского тега, на случай, 
+    //если он там есть
     this.elements.listElem.textContent = ''
 
+    //Перебор постов из аргументов, вызывающий последовательное
+    //добавление верстки поста внутрь тега перед его концом, а значит
+    //друг за другом
     posts.forEach(({id, 
       userName, 
-      nickname, 
+      nickname,
+      
+      //передается функция для формата даты
       getDate, 
+
       text, 
       img, 
       likes}) => {
@@ -83,34 +111,53 @@ class Twitter {
     })
   }
 
+  //Следующие функции отображения используют вызов
+  //функции this.renderPosts c передачей списка
+  //конкретных постов, отвечающих смыслу данной функции 
+
+  //Отображение постов пользователя
   showUserPost() {
 
   }
 
+  //хз
   showLikesPost() {
 
   }
 
+  //Отображение всех постов 
   showAllPosts() {
     this.renderPosts(this.tweets.posts)
   }
 
+  ////////////////
+
+  //Открытие модального окна(?)
   openModal(){
 
   }
 }
 
+//Класс для списка постов (объектов класса Post), а так же работы
+//с этим списком и элементами по отдельности
 class Posts {
   constructor({posts = []} = {}) {
+
+    //Список постов (объектов класса Post)
     this.posts = posts
   }
 
+  //Функция добавления поста в список. Принимает внешнее окружение,
+  //поэтому может передаваться для работы как функция
+  // в наследующие классы
   addPost = (post) => {
     this.posts.push(new Post(post)) 
   }
-  
+
+  //Удаление поста из списка
   deletePost(id) { 
     try {
+      //Да вроде не сложно и в одну строчку :)
       this.posts.splice(this.posts.findIndex(post => post.id === `${id}`), 1)
     } catch(err) {
       if (err) console.log('wrong id, cannot delete') 
@@ -118,8 +165,10 @@ class Posts {
     
   }
 
+  //Лайкнуть пост, используя Post.changeLike, метод класса Post
   likePost(id) {
     try {
+      //Да вроде не сложно и в одну строчку :)
       this.posts[this.posts.findIndex(post => post.id === `${id}`)].changeLike()
     } catch(err) {
       if (err) console.log('wrong id, cannot delete') 
@@ -127,20 +176,35 @@ class Posts {
   }
 }
 
+//Класс для структуры отдельного поста и 
+//функционала для воздействия на него
 class Post {
   constructor(param) {
-    this.id = param.id || this.generateId()
-    this.userName = param.userName || 'empty'
-    this.nickname = param.nickname || 'empty'
-    this.postDate = new Date(param.postDate) || new Date()
-    this.text = param.text || 'empty'
-    this.img = param.img || 'none'
-    this.likes = param.likes || 0
-    this.liked = false
+    
+    //При создании характеристик используется заполнение по
+    //умолчанию через (переданный параметр) || (параметр по умолчанию)
+    
+    // return a || b /-/ если а - true, вернется a, иначе
+    // если b - true, вернется b, иначе вернется fasle
+    // в нашем случае b точно true, поэтому вернется либо
+    // a, либо b, значит, каждая характеристика будет заполнена
+    
+    
+    this.id = param.id || this.generateId()                  //id поста
+    this.userName = param.userName || 'empty'                //имя пользователя
+    this.nickname = param.nickname || 'empty'                //ник пользователя
+    this.postDate = new Date(param.postDate) || new Date()   //дата поста
+    this.text = param.text || 'empty'                        //текст поста
+    this.img = param.img || 'none'                           //путь к изображению поста
+    this.likes = param.likes || 0                            //лайки поста
+    this.liked = false                                       //последнее действие с лайком (true - поставлен лайк, false - убран)
   }
 
+  //Добавляет лайк, либо убирает (по сути нет работы с Юзером,
+  // поэтому функция будет добавлять-убирать только один лайк,
+  //нуждается в доработке)
   changeLike() {
-    this.like = !this.like
+    this.liked = !this.liked
     if (this.like){
       this.likes++
     } else {
@@ -148,10 +212,12 @@ class Post {
     }
   }
 
+  //Функция для создания id по умолчанию
   generateId() {
     return (Math.random().toString(32).substring(2, 9) + (+new Date).toString(32))
   }
 
+  //Функция для форматирования даты
   getDate = () => {
 
     const options = {
